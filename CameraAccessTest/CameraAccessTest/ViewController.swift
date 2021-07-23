d0bf0f4c8115fbe061beb22d58aa02a7a41f0590
James L <59751464+Jazz1016@users.noreply.github.com>
2021-07-23 17:42:52 -0600

    got it working

--- CameraAccessTest/CameraAccessTest/ViewController.swift
+++ CameraAccessTest/CameraAccessTest/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARK: - Outlets
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var button: UIButton!
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imageView.backgroundColor = .secondarySystemBackground
+        
+        button.backgroundColor = .systemBlue
+        button.setTitle("Take Picture", for: .normal)
+        button.setTitleColor(.white, for: .normal)
     }
-
+    
+    // MARK: - Actions
+    @IBAction func didTapButton(){
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true, completion: nil)
+    }
 
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        picker.dismiss(animated: true, completion: nil)
+        
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {return}
+        imageView.image = image
+    }
+    
+    
+}
