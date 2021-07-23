7d38e3019ca742643a150a79994f0385c4f64e04
James L <59751464+Jazz1016@users.noreply.github.com>
2021-07-23 17:03:11 -0600

    first commit

---
+++ CameraAccessTest/CameraAccessTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CameraAccessTest
+//
+//  Created by James Lea on 7/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
